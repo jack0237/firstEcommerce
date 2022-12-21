cfdd96267a01aaccd507c9dea0a68a853aa3857d
jack0237 <99824627+jack0237@users.noreply.github.com>
2022-12-21 11:35:27 +0100

    first comit on final project python

--- daily_food/models.py
+++ daily_food/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=200)
     price = models.FloatField(max_length=20)
-    description = models.TextField(max_length=200)
+    description = models.TextField(max_length=1000000)
     category = models.ForeignKey (Category, related_name ='categorie', on_delete = models.CASCADE)
     image = models.CharField(max_length=500000)
     date_added = models.DateTimeField(auto_now=True)
--- daily_food/urls.py
+++ daily_food/urls.py
@@ -1,12 +1,13 @@
 from django import views
 from django.urls import path,include
-from accounts.views import signup
+from accounts.views import signup, logout_user, login_user
 from .views import *
 
 urlpatterns = [
     path('', index, name="home"),
     path('<int:myid>', detail, name="detail"), # url dynamique qui change selon les id pour aficher les details des produits
     path('signup/', signup, name="signup"),
-    path('login',login, name="login"),
+    path('logout/', logout_user, name="logout"),
+    path('login/',login_user, name="login"),
 
 ]
